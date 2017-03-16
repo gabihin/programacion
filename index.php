65b3dc0215cb0731a8e2e0ac0449f407f938ba8d
Gabriela Hinrichsen <gabihin@Gabrielas-MacBook-Pro.local>
2017-03-16 19:05:59 +0100

    form nombre

--- index.php
+++ index.php
@@ -5,7 +5,22 @@
 </head>
 
 <body>
+<?php
 
+	$nombre= "Gabriela";
+	if (isset($_POST[ 'nombre'])) {
+		$nombre=$_POST['nombre'];
+	}
+	echo "Hola, ".$nombre;
+
+
+
+?>
+ 
+<form action="index.php" method="post">
+	<input type="text" name="nombre" placeholder="escribe tu nombre">
+	<br>
+	<input type="submit" value="enviar">
 
 </body>
 </html>
